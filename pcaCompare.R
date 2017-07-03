468c7ed01fa8d4cd4a21d2119aaa8733b6f9111b
SijieGuo <115107477@umail.ucc.ie>
2017-07-03 13:59:50 +0100

    Update pcaCompare.R

--- pcaCompare.R
+++ pcaCompare.R
@@ -3,7 +3,6 @@ compPCA <- function(dataset, pca1, pca2, minDims,maxDims)
 {
   #load functions
   source("./pcaAccuracy.R")
-  #load library
   pcaFirst <- pcaAccuracy(pca1, dataset, minDims, maxDims)
   pcaSecond <- pcaAccuracy(pca2, dataset, minDims, maxDims)
   difference <- pcaFirst - pcaSecond
